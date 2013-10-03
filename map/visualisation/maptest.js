270e2345ab85a7c21cf8aba6965cdcc1b4622703
Johan Gant <johan.gant@torchbox.com>
2013-10-03 10:17:16 +0100

    map test

---
+++ map/visualisation/maptest.js
@@ -0,0 +1,191 @@
+[
+{
+  "width": 784, "height": 551,
+  "boundaries": "country-sha",
+  "borderWidth": 1,
+  "borderColour": "#9fa2a3",
+  "legendTitle": "Proportion of PCT secondary care budgets spent on services from ISPs (aggregated to SHA level)",
+  "tabTitle": "2011/12",
+  "opacity":0.8,
+  "descriptionPrefix": "Percentage of secondary care budget: ",
+  "descriptionSuffix": "%",
+  "preserveViewport": false,
+  "styles": [
+    {"max": 4, "fillColour": "#D5D1C8", "label": "2% to 4%"},
+    {"max": 6, "fillColour": "#55504C", "label": "4% to 6%"},
+    {"max": 8, "fillColour": "#2B505E", "label": "6% to 8%"},
+    {"max": 10, "fillColour": "#495B11", "label": "8% to 10%"},
+    {"max": 12, "fillColour": "#3C1F75", "label": "10% to 12%"}
+  ],
+  "data": {
+    "Q30": 4.6,
+    "Q31": 6.3,
+    "Q32": 9.3,
+    "Q33": 10.5,
+    "Q34": 8.1,
+    "Q35": 7.6,
+    "Q36": 5.5,
+    "Q37": 9.6,
+    "Q38": 9.3,
+    "Q39": 9.0
+  }
+},
+{
+  "width": 784, "height": 551,
+  "boundaries": "country-sha",
+  "borderWidth": 1,
+  "borderColour": "#9fa2a3",
+  "legendTitle": "Proportion of PCT secondary care budgets spent on services from ISPs (aggregated to SHA level)",
+  "tabTitle": "2010/11",
+  "opacity":0.8,
+  "descriptionPrefix": "Percentage of secondary care budget: ",
+  "descriptionSuffix": "%",
+  "preserveViewport": false,
+  "styles": [
+    {"max": 4, "fillColour": "#D5D1C8", "label": "2% to 4%"},
+    {"max": 6, "fillColour": "#55504C", "label": "4% to 6%"},
+    {"max": 8, "fillColour": "#2B505E", "label": "6% to 8%"},
+    {"max": 10, "fillColour": "#495B11", "label": "8% to 10%"},
+    {"max": 12, "fillColour": "#3C1F75", "label": "10% to 12%"}
+  ],
+  "data": {
+    "Q30": 4.2,
+    "Q31": 6.1,
+    "Q32": 8.1,
+    "Q33": 9.8,
+    "Q34": 8.2,
+    "Q35": 5.7,
+    "Q36": 4.6,
+    "Q37": 9.1,
+    "Q38": 8.9,
+    "Q39": 7.5
+  }
+},
+{
+  "width": 784, "height": 551,
+  "boundaries": "country-sha",
+  "borderWidth": 1,
+  "borderColour": "#9fa2a3",
+  "legendTitle": "Proportion of PCT secondary care budgets spent on services from ISPs (aggregated to SHA level)",
+  "tabTitle": "2009/10",
+  "opacity":0.8,
+  "descriptionPrefix": "Percentage of secondary care budget: ",
+  "descriptionSuffix": "%",
+  "preserveViewport": false,
+  "styles": [
+    {"max": 4, "fillColour": "#D5D1C8", "label": "2% to 4%"},
+    {"max": 6, "fillColour": "#55504C", "label": "4% to 6%"},
+    {"max": 8, "fillColour": "#2B505E", "label": "6% to 8%"},
+    {"max": 10, "fillColour": "#495B11", "label": "8% to 10%"},
+    {"max": 12, "fillColour": "#3C1F75", "label": "10% to 12%"}
+  ],
+  "data": {
+    "Q30": 3.8,
+    "Q31": 6.6,
+    "Q32": 6.8,
+    "Q33": 8.4,
+    "Q34": 6.9,
+    "Q35": 5.1,
+    "Q36": 4.4,
+    "Q37": 8.6,
+    "Q38": 8.4,
+    "Q39": 6.8
+  }
+},
+{
+  "width": 784, "height": 551,
+  "boundaries": "country-sha",
+  "borderWidth": 1,
+  "borderColour": "#9fa2a3",
+  "legendTitle": "Proportion of PCT secondary care budgets spent on services from ISPs (aggregated to SHA level)",
+  "tabTitle": "2008/09",
+  "opacity":0.8,
+  "descriptionPrefix": "Percentage of secondary care budget: ",
+  "descriptionSuffix": "%",
+  "preserveViewport": false,
+  "styles": [
+    {"max": 4, "fillColour": "#D5D1C8", "label": "2% to 4%"},
+    {"max": 6, "fillColour": "#55504C", "label": "4% to 6%"},
+    {"max": 8, "fillColour": "#2B505E", "label": "6% to 8%"},
+    {"max": 10, "fillColour": "#495B11", "label": "8% to 10%"},
+    {"max": 12, "fillColour": "#3C1F75", "label": "10% to 12%"}
+  ],
+  "data": {
+    "Q30": 3.2,
+    "Q31": 6.2,
+    "Q32": 6.2,
+    "Q33": 7.1,
+    "Q34": 6.6,
+    "Q35": 5.3,
+    "Q36": 4.5,
+    "Q37": 9.1,
+    "Q38": 7.7,
+    "Q39": 5.6
+  }
+},
+
+{
+  "width": 784, "height": 551,
+  "boundaries": "country-sha",
+  "borderWidth": 1,
+  "borderColour": "#9fa2a3",
+  "legendTitle": "Proportion of PCT secondary care budgets spent on services from ISPs (aggregated to SHA level)",
+  "tabTitle": "2007/08",
+  "opacity":0.8,
+  "descriptionPrefix": "Percentage of secondary care budget: ",
+  "descriptionSuffix": "%",
+  "preserveViewport": false,
+  "styles": [
+    {"max": 4, "fillColour": "#D5D1C8", "label": "2% to 4%"},
+    {"max": 6, "fillColour": "#55504C", "label": "4% to 6%"},
+    {"max": 8, "fillColour": "#2B505E", "label": "6% to 8%"},
+    {"max": 10, "fillColour": "#495B11", "label": "8% to 10%"},
+    {"max": 12, "fillColour": "#3C1F75", "label": "10% to 12%"}
+  ],
+  "data": {
+    "Q30": 3.3,
+    "Q31": 5.4,
+    "Q32": 5.6,
+    "Q33": 5.7,
+    "Q34": 6.5,
+    "Q35": 4.6,
+    "Q36": 4.3,
+    "Q37": 8.2,
+    "Q38": 7.4,
+    "Q39": 5.7
+  }
+},
+
+{
+  "width": 784, "height": 551,
+  "boundaries": "country-sha",
+  "borderWidth": 1,
+  "borderColour": "#9fa2a3",
+  "legendTitle": "Proportion of PCT secondary care budgets spent on services from ISPs (aggregated to SHA level)",
+  "tabTitle": "2006/07",
+  "opacity":0.8,
+  "descriptionPrefix": "Percentage of secondary care budget: ",
+  "descriptionSuffix": "%",
+  "preserveViewport": false,
+  "styles": [
+    {"max": 4, "fillColour": "#D5D1C8", "label": "2% to 4%"},
+    {"max": 6, "fillColour": "#55504C", "label": "4% to 6%"},
+    {"max": 8, "fillColour": "#2B505E", "label": "6% to 8%"},
+    {"max": 10, "fillColour": "#495B11", "label": "8% to 10%"},
+    {"max": 12, "fillColour": "#3C1F75", "label": "10% to 12%"}
+  ],
+  "data": {
+    "Q30": 2.3,
+    "Q31": 3.3,
+    "Q32": 4,
+    "Q33": 3.9,
+    "Q34": 3.7,
+    "Q35": 4.8,
+    "Q36": 3.4,
+    "Q37": 4.9,
+    "Q38": 5.8,
+    "Q39": 4.5
+
+  }
+}
+]
