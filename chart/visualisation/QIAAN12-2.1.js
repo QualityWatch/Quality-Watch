648cfc8763480a46a242aa367f0986306e812718
johan gant <johan.gant@gmail.com>
2013-10-16 11:59:27 +0100

    replaced copyright symbol with &copy;

--- chart/visualisation/QIAAN12-2.1.js
+++ chart/visualisation/QIAAN12-2.1.js
@@ -3,7 +3,7 @@ title: {text:''},
 credits: {enabled: false},
 exporting: {chartOptions: {
 title: {text: 'How do overall number of medication safety incidents reported change by sector?'},
-credits: {enabled: true, text: '© Nuffield Trust & The Health Foundation', href: 'http://www.qualitywatch.org.uk'}
+credits: {enabled: true, text: '&copy; Nuffield Trust & The Health Foundation', href: 'http://www.qualitywatch.org.uk'}
 }},xAxis: {
 title: {text:''}
 , categories: ['Oct 09 - Mar 10','Apr 11 - Sep 11','Oct 11 - Mar 12']
