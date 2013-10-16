636528fec217d6a447ccd23653ef985ed70d0e23
QualityWatch <Ian.blunt@nuffieldtrust.org.uk>
2013-10-16 20:30:11 +0100

    Update QIAAN99-1.js

--- chart/visualisation/QIAAN99-1.js
+++ chart/visualisation/QIAAN99-1.js
@@ -3,7 +3,7 @@ title: {text:''},
 credits: {enabled: false},
 exporting: {chartOptions: {
 title: {text: 'How have NHS staff numbers changed by staff type?'},
-credits: {enabled: true, text: '© Nuffield Trust & The Health Foundation', href: 'http://www.qualitywatch.org.uk'}
+credits: {enabled: true, text: '&copy; Nuffield Trust & The Health Foundation', href: 'http://www.qualitywatch.org.uk'}
 }},
 xAxis: {
 title: {text:''}
