0508a99fb760b1a62369aaeecb1d0714646bfc99
QualityWatch <Ian.blunt@nuffieldtrust.org.uk>
2013-10-16 20:20:43 +0100

    Update QIAAN81-1.js

--- chart/visualisation/QIAAN81-1.js
+++ chart/visualisation/QIAAN81-1.js
@@ -3,7 +3,7 @@ title: {text:''},
 credits: {enabled: false},
 exporting: {chartOptions: {
 title: {text: 'How has the proportion of people receiving diagnostic tests within 6 and 13 weeks changed?'},
-credits: {enabled: true, text: '© Nuffield Trust & The Health Foundation', href: 'http://www.qualitywatch.org.uk'}
+credits: {enabled: true, text: '&copy; Nuffield Trust & The Health Foundation', href: 'http://www.qualitywatch.org.uk'}
 }},
 xAxis: {
 title: {text:''}
