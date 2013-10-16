6884274d702474bce63a63aa8e91d1ef7495f1dd
QualityWatch <Ian.blunt@nuffieldtrust.org.uk>
2013-10-16 20:28:08 +0100

    Update QIAAN96-2.js

--- chart/visualisation/QIAAN96-2.js
+++ chart/visualisation/QIAAN96-2.js
@@ -3,7 +3,7 @@ title: {text:''},
 credits: {enabled: false},
 exporting: {chartOptions: {
 title: {text: 'How has the proportion of people dying in their usual residence changed by region?'},
-credits: {enabled: true, text: '© Nuffield Trust & The Health Foundation', href: 'http://www.qualitywatch.org.uk'}
+credits: {enabled: true, text: '&copy; Nuffield Trust & The Health Foundation', href: 'http://www.qualitywatch.org.uk'}
 }},
 xAxis: {
 title: {text:''}
