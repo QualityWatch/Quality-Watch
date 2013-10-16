cbfc018a623d06433589618e2656f496c2f12a70
QualityWatch <Ian.blunt@nuffieldtrust.org.uk>
2013-10-16 20:18:43 +0100

    Update QIAAN78-2.js

--- chart/visualisation/QIAAN78-2.js
+++ chart/visualisation/QIAAN78-2.js
@@ -3,7 +3,7 @@ title: {text:''},
 credits: {enabled: false},
 exporting: {chartOptions: {
 title: {text: 'How has waiting times after an urgent referral changed by cancer type?'},
-credits: {enabled: true, text: '© Nuffield Trust & The Health Foundation', href: 'http://www.qualitywatch.org.uk'}
+credits: {enabled: true, text: '&copy; Nuffield Trust & The Health Foundation', href: 'http://www.qualitywatch.org.uk'}
 }},
 xAxis: {
 title: {text:'Type of cancer'}
