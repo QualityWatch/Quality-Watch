affeacfe58cec65fa8227f1ab325d81305ff25bd
QualityWatch <Ian.blunt@nuffieldtrust.org.uk>
2013-10-28 10:14:36 +0000

    Update QIAAN95-1.js

--- chart/visualisation/QIAAN95-1.js
+++ chart/visualisation/QIAAN95-1.js
@@ -3,7 +3,7 @@ title: {text:''},
 credits: {enabled: false},
 exporting: {chartOptions: {
 title: {text: 'How many elective operations have been cancelled and how has this changed?'},
-credits: {enabled: true, text: '© Nuffield Trust & The Health Foundation', href: 'http://www.qualitywatch.org.uk'}
+credits: {enabled: true, text: 'Copyright Nuffield Trust & The Health Foundation', href: 'http://www.qualitywatch.org.uk'}
 }},
 xAxis: {
 title: {text:''}
