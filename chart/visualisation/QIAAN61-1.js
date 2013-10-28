905341e2b17bb57d71f37f244218e2ec0ee76be6
QualityWatch <Ian.blunt@nuffieldtrust.org.uk>
2013-10-28 11:16:10 +0000

    Update QIAAN61-1.js

--- chart/visualisation/QIAAN61-1.js
+++ chart/visualisation/QIAAN61-1.js
@@ -3,7 +3,7 @@ title: {text:''},
 credits: {enabled: false},
 exporting: {chartOptions: {
 title: {text: 'Do doctors or nurses talk in front of patients as though they weren\'t there?'},
-credits: {enabled: true, text: '© Nuffield Trust & The Health Foundation', href: 'http://www.qualitywatch.org.uk'}
+credits: {enabled: true, text: 'Copyright Nuffield Trust & The Health Foundation', href: 'http://www.qualitywatch.org.uk'}
 }},
 xAxis: {
 title: {text:''}
