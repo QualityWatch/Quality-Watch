81ec9130663d527a433836846b259791def99ff6
QualityWatch <Ian.blunt@nuffieldtrust.org.uk>
2013-10-28 10:20:46 +0000

    Update QIAAN80-1.js

--- chart/visualisation/QIAAN80-1.js
+++ chart/visualisation/QIAAN80-1.js
@@ -3,7 +3,7 @@ title: {text:''},
 credits: {enabled: false},
 exporting: {chartOptions: {
 title: {text: 'How have referral to treatment times changed by pathway?'},
-credits: {enabled: true, text: '© Nuffield Trust & The Health Foundation', href: 'http://www.qualitywatch.org.uk'}
+credits: {enabled: true, text: 'Copyright Nuffield Trust & The Health Foundation', href: 'http://www.qualitywatch.org.uk'}
 }},
 xAxis: {
 title: {text:''}
