2bf83a90eef34c42c68773bc08c2dbdb740416a6
QualityWatch <Ian.blunt@nuffieldtrust.org.uk>
2013-10-28 11:22:30 +0000

    Update QIAAN36-1.js

--- chart/visualisation/QIAAN36-1.js
+++ chart/visualisation/QIAAN36-1.js
@@ -3,7 +3,7 @@ title: {text:''},
 credits: {enabled: false},
 exporting: {chartOptions: {
 title: {text: 'What do staff say about support from their managers and how has it changed?'},
-credits: {enabled: true, text: '© Nuffield Trust & The Health Foundation', href: 'http://www.qualitywatch.org.uk'}
+credits: {enabled: true, text: 'Copyright Nuffield Trust & The Health Foundation', href: 'http://www.qualitywatch.org.uk'}
 }},
 xAxis: {
 title: {text:''}
