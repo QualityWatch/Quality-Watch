7154808553fa5cf33dc44fb73b4fc8d1d7753dbe
QualityWatch <Ian.blunt@nuffieldtrust.org.uk>
2013-10-28 11:27:50 +0000

    Update QIAAN101-1.js

--- chart/visualisation/QIAAN101-1.js
+++ chart/visualisation/QIAAN101-1.js
@@ -3,7 +3,7 @@ title: {text:''},
 credits: {enabled: false},
 exporting: {chartOptions: {
 title: {text: 'How have mortality rates changed with respect to deprivation?'},
-credits: {enabled: true, text: '© Nuffield Trust & The Health Foundation', href: 'http://www.qualitywatch.org.uk'}
+credits: {enabled: true, text: 'Copyright Nuffield Trust & The Health Foundation', href: 'http://www.qualitywatch.org.uk'}
 }},
 xAxis: {
 title: {text:'Index of multiple deprivation (IMD) score 2007'}
