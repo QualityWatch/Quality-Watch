e991c8d524958229ae4661f60beeb48d61664576
QualityWatch <Ian.blunt@nuffieldtrust.org.uk>
2013-10-28 11:18:04 +0000

    Update QIAAN53-QIAAN54-QIAAN87-QIAAN55-1.js

--- chart/visualisation/QIAAN53-QIAAN54-QIAAN87-QIAAN55-1.js
+++ chart/visualisation/QIAAN53-QIAAN54-QIAAN87-QIAAN55-1.js
@@ -3,7 +3,7 @@ title: {text:''},
 credits: {enabled: false},
 exporting: {chartOptions: {
 title: {text: 'How have outpatients\' experiences changed over time?'},
-credits: {enabled: true, text: '© Nuffield Trust & The Health Foundation', href: 'http://www.qualitywatch.org.uk'}
+credits: {enabled: true, text: 'Copyright Nuffield Trust & The Health Foundation', href: 'http://www.qualitywatch.org.uk'}
 }},
 xAxis: {
 title: {text:''}
