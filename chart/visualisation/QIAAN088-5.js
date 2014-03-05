2ad2f8c205728af26c8562f522132d3172facf61
qualityWatch <ian.blunt@nuffieldtrust.org.uk>
2014-03-05 10:49:01 +0000

    Fix to ambs

---
+++ chart/visualisation/QIAAN088-5.js
@@ -0,0 +1,56 @@
+ {
+        title: {
+            text: ''
+        },
+        credits: {
+            enabled: false
+        },
+        exporting: {
+            chartOptions: {
+                title: {
+                    text: 'How have trusts breaching the 8 minute target for Red 1 and 2 calls changed over time?'
+                },
+                credits: {
+                    enabled: true,
+                    text: 'Copyright Nuffield Trust & The Health Foundation',
+                    href: 'http://www.qualitywatch.org.uk'
+                }
+            }
+        },
+        xAxis: {
+            title: {
+                text: ''
+            },
+            categories: ['Jun 2012', 'Jul 2012', 'Aug 2012', 'Sep 2012', 'Oct 2012', 'Nov 2012', 'Dec 2012', 'Jan 2013', 'Feb 2013', 'Mar 2013', 'Apr 2013', 'May 2013', 'Jun 2013', 'Jul 2013', 'Aug 2013', 'Sep 2013', 'Oct 2013', 'Nov 2013', 'Dec 2013'],
+            labels: {
+                rotation: -90,
+                step: 1
+            }
+        },
+        yAxis: {
+            title: {
+                text: 'Number of trusts '
+            },
+            max: 14,
+            labels: {}
+        },
+        plotOptions: {
+                column: {
+                    stacking: 'normal'}},
+        series: [{
+            name: 'meeting the 8 minute target for responding to category A calls',
+            type: 'column',
+            data: [11, 11, 12, 10, 11, 9, 2, 8, 6, 4, 8, 10, 9, 4, 5, 5, 5, 3, 5],
+            index: 1
+        }, {
+            name: 'breaching the 8 minute target for responding to category A calls',
+            type: 'column',
+            data: [1, 1, 0, 2, 1, 3, 10, 4, 5, 7, 3, 1, 2, 7, 6, 6, 6, 8, 6],
+            index: 2
+        }, {
+            name: 'National breach',
+            type: 'line',
+            data: [null, null, null, null, null, null, 13,null, null, 13,null, null, null, 13,null, 13,13,13,13],
+            index: 1
+        }]
+    }
