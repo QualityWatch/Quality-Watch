cb48ce7895434bce1d2fd3658f2f88f125f52005
QualityWatch <Ian.blunt@nuffieldtrust.org.uk>
2014-06-16 12:45:54 +0100

    Update QIAAN103-3.js

--- chart/visualisation/QIAAN103-3.js
+++ chart/visualisation/QIAAN103-3.js
@@ -1,4 +1,5 @@
-/* Created by ianb on 29/05/2014 14:46:30 using v2.1 */{
+/* Created by ianb on 29/05/2014 14:46:30 using v2.1 */
+{
 title: {text:''},
 credits: {enabled: false},
 exporting: {chartOptions: {
@@ -7,7 +8,8 @@ credits: {enabled: true, text: 'Copyright Nuffield Trust & The Health Foundation
 }},
 xAxis: {
 title: {text:''}
-, categories: ['January','January','January','January','February','February','February','February','March','March','March','March','March','April','April','April','April','May','May','May','May','June','June','June','June','June','July','July','July','July','August','August','August','August','September','September','September','September','September','October','October','October','October','November','November','November','November','December','December','December','December','December']
+, categories: ['Week 1 (Jan)', 'Week 2 (Jan)', 'Week 3 (Jan)', 'Week 4 (Jan)', 'Week 5 (Feb)', 'Week 6 (Feb)',
+'Week 7 (Feb)', 'Week 8 (Feb)', 'Week 9 (Mar)', 'Week 10 (Mar)', 'Week 11 (Mar)', 'Week 12 (Mar)', 'Week 13 (Mar)', 'Week 14 (Apr)', 'Week 15 (Apr)', 'Week 16 (Apr)', 'Week 17 (Apr)','Week 18 (May)','Week 19 (May)','Week 20 (May)','Week 21 (May)','Week 22 (Jun)','Week 23 (Jun)','Week 24 (Jun)','Week 25 (Jun)','Week 26 (Jun)','Week 27 (Jul)','Week 28 (Jul)','Week 29 (Jul)','Week 30 (Jul)','Week 31 (Aug)','Week 32 (Aug)','Week 33 (Aug)','Week 34 (Aug)','Week 35 (Sep)','Week 36 (Sep)','Week 37 (Sep)','Week 38 (Sep)','Week 39 (Sep)','Week 40 (Oct)','Week 41 (Oct)','Week 42 (Oct)','Week 43 (Oct)','Week 44 (Nov)','Week 45 (Nov)','Week 46 (Nov)','Week 47 (Nov)','Week 48 (Dec)','Week 49 (Dec)','Week 50 (Dec)','Week 51 (Dec)','Week 52 (Dec)']
 , labels: {
  rotation: -90
 , step: 2
