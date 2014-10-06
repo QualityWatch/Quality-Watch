064e9b99dd2d99dcad18977cde9a8455f4afedea
QualityWatch <Ian.blunt@nuffieldtrust.org.uk>
2014-10-06 17:05:27 +0100

    Update QIAAN121-3.js

--- chart/visualisation/QIAAN121-3.js
+++ chart/visualisation/QIAAN121-3.js
@@ -1,4 +1,5 @@
-/* Created by EFisher on 26/03/2014 17:20:41 using v2.1 */{
+/* Created by EFisher on 26/03/2014 17:20:41 using v2.1 */
+   {
 title: {text:''},
 credits: {enabled: false},
 exporting: {chartOptions: {
@@ -7,7 +8,7 @@ credits: {enabled: true, text: 'Copyright Nuffield Trust & The Health Foundation
 }},
 xAxis: {
 title: {text:''}
-, categories: [2010,2011,2012,2013]
+, categories: [2010,2011,2012,2013,2014]
 , labels: {
 }
 },
@@ -23,20 +24,17 @@ series:[
 {
 name: 'Yes, definitely',
 type: 'column',
-data: [88,87,87,86]
-,index: 2
+    data: [88,87,87,86, {y: 75, color:'#3FCFD5'}]
 },
 {
 name: 'Yes, to some extent',
 type: 'column',
-data: [10,11,11,11]
-,index: 1
+    data: [10,11,11,11, {y: 19, color:'#E59AAA'}]
 },
 {
 name: 'No',
 type: 'column',
-data: [2,2,2,2]
-,index: 0
+    data: [2,2,2,2, {y:6, color:'#B634BB'}]
 }
 ]
 }
