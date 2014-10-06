c2d622dcbb12706def9d706bb64302a88d1b91aa
QualityWatch <Ian.blunt@nuffieldtrust.org.uk>
2014-10-06 16:14:40 +0100

    Update QIAAN78-3.js

--- chart/visualisation/QIAAN78-3.js
+++ chart/visualisation/QIAAN78-3.js
@@ -1,41 +1,66 @@
-/* Created by ianb on 01/10/2014 11:51:59 using v2.1 */{
-title: {text:''},
-credits: {enabled: false},
-exporting: {chartOptions: {
-title: {text: 'How have waiting times after an urgent referral changed for suspected breast cancer?'},
-credits: {enabled: true, text: 'Copyright Nuffield Trust & The Health Foundation', href: 'http://www.qualitywatch.org.uk'}
-}},
-xAxis: {
-title: {text:''}
-, categories: ['2012/13 Q1','2012/13 Q2','2012/13 Q3','2012/13 Q4','2013/14 Q1','2013/14 Q2','2013/14 Q3','2013/14 Q4','2014/15 Q1']
-, labels: {
- rotation: -90
-, step: 1
-}
-},
-yAxis: {
-title: {text:'Number of people'}
-, labels: {
-format: '{value:.1f}' 
-}},
-series:[
 {
-name: 'Proportion of people seen within 2 weeks - suspected cancer',
-type: 'line',
-data: [1,1,1,1,1,1,1,1,0.9]
-,index: 1
-},
-{
-name: 'Number of people with an urgent referral for suspected breast cancer',
-type: 'line',
-data: [53362,51514,58966,56203,58535,60969,62868,70187,71876]
-,index: 1
-},
-{
-name: 'Number of people seen within 2 weeks following referral for suspected breast cancer',
-type: 'line',
-data: [51853,50197,57408,54623,56666,58543,60825,67295,66368]
-,index: 2
-}
-]
-}
+        title: {
+            text: ''
+        },
+        credits: {
+            enabled: false
+        },
+        exporting: {
+            chartOptions: {
+                title: {
+                    text: 'How have waiting times after an urgent referral changed for suspected breast cancer?'
+                },
+                credits: {
+                    enabled: true,
+                    text: 'Copyright Nuffield Trust & The Health Foundation',
+                    href: 'http://www.qualitywatch.org.uk'
+                }
+            }
+        },
+        xAxis: {
+            title: {
+                text: ''
+            },
+            categories: ['2012/13 Q1', '2012/13 Q2', '2012/13 Q3', '2012/13 Q4', '2013/14 Q1', '2013/14 Q2', '2013/14 Q3', '2013/14 Q4', '2014/15 Q1'],
+            labels: {
+                rotation: -90,
+                step: 1
+            }
+        },
+        yAxis: [{ min: 0,
+            title: {
+                text: 'Number of people with an urgent referral for suspected breast cancer'
+            },
+            labels: {
+                format: '{value:.1f}'
+            }
+        }, {
+            max: 100,
+            title: {
+                text: 'Percentage'
+            },
+            labels: {
+                format: '{value:.1f}'
+            },
+            opposite: true
+        }],
+
+
+        tooltip: {
+            shared: true
+        },
+        series: [{
+            name: 'Percentage seen within 2 weeks',
+            type: 'line',
+            yAxis: 1,
+            data: [97.2,97.4,97.4,97.2,96.8,96,96.8,95.88,92.34]
+        }, {
+            name: 'Number of people',
+            type: 'line',
+            data: [53362, 51514, 58966, 56203, 58535, 60969, 62868, 70187, 71876]
+        }, {
+            name: 'Number of people seen within 2 weeks',
+            type: 'line',
+            data: [51853, 50197, 57408, 54623, 56666, 58543, 60825, 67295, 66368]
+        }]
+    }
