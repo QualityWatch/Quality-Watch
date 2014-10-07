96995e60abfe8e4fedb10dfc247461530eca72fb
QualityWatch <Ian.blunt@nuffieldtrust.org.uk>
2014-10-07 13:17:26 +0100

    Update QIAAN78-3.js

--- chart/visualisation/QIAAN78-3.js
+++ chart/visualisation/QIAAN78-3.js
@@ -1,4 +1,4 @@
-{
+    {
         title: {
             text: ''
         },
@@ -27,14 +27,14 @@
                 step: 1
             }
         },
-        yAxis: [{ min: 0,
+        yAxis: [{
+            min: 0,
             title: {
-                text: 'Number of people with an urgent referral for suspected breast cancer'
-            },
-            labels: {
-                format: '{value:.1f}'
-            }
-        }, {
+                text: 'Number of people with an urgent <br> referral for suspected breast cancer',
+                offset: 87},
+        labels: {format: '{value:.1f}'}
+        },
+        {
             max: 100,
             title: {
                 text: 'Percentage'
@@ -46,21 +46,21 @@
         }],
 
 
-        tooltip: {
-            shared: true
-        },
-        series: [{
-            name: 'Percentage seen within 2 weeks',
-            type: 'line',
-            yAxis: 1,
-            data: [97.2,97.4,97.4,97.2,96.8,96,96.8,95.88,92.34]
-        }, {
-            name: 'Number of people',
-            type: 'line',
-            data: [53362, 51514, 58966, 56203, 58535, 60969, 62868, 70187, 71876]
-        }, {
-            name: 'Number of people seen within 2 weeks',
-            type: 'line',
-            data: [51853, 50197, 57408, 54623, 56666, 58543, 60825, 67295, 66368]
-        }]
+    tooltip: {
+        shared: true
+    },
+    series: [{
+        name: 'Number of people',
+        type: 'column',
+        data: [53362, 51514, 58966, 56203, 58535, 60969, 62868, 70187, 71876]
+    }, {
+        name: 'Number of people seen within 2 weeks',
+        type: 'column',
+        data: [51853, 50197, 57408, 54623, 56666, 58543, 60825, 67295, 66368]
+    }, {
+        name: 'Percentage seen within 2 weeks',
+        type: 'line',
+        yAxis: 1,
+        data: [97.2, 97.4, 97.4, 97.2, 96.8, 96, 96.8, 95.88, 92.34]
+    }]
     }
