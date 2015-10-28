da8a82e63a20b21ade52be48d20b2431c7e7528f
QualityWatch <Feedback@qualitywatch.org.uk>
2015-10-28 16:28:09 +0000

    Update QIAAN126-3.js

--- chart/visualisation/QIAAN126-3.js
+++ chart/visualisation/QIAAN126-3.js
@@ -1,4 +1,5 @@
-/* Created by EFisher on 26/10/2015 10:08:30 using v2.7 */{
+/* Created by EFisher on 26/10/2015 10:08:30 using v2.7 */
+{
 title: {text:''},
 credits: {enabled: false},
 exporting: {chartOptions: {
@@ -22,31 +23,31 @@ series:[
 {
 name: 'Residential',
 type: 'column',
-data: [2390.2,2225.9,2183.8,2328.7]
+data: [23.902,22.259,21.838,23.287]
 ,index: 4
 },
 {
 name: 'Day',
 type: 'column',
-data: [1022.6,980.6,1089.2,1017.1]
-,index: 3
+data: [10.226,9.806,10.892,10.171]
+    ,index: 3
 },
 {
 name: 'Domiciliary (home care)',
 type: 'column',
-data: [1449.2,1269.1,1410.4,1365]
+data: [14.492,12.691,14.104,13.65]
 ,index: 2
 },
 {
 name: 'Community',
 type: 'column',
-data: [3174.4,3492.5,3268.4,3317.8]
+data: [31.744,34.925,32.684,33.178]
 ,index: 1
 },
 {
 name: 'Other',
 type: 'column',
-data: [1963.6,2031.8,2048.2,1971.4]
+data: [19.636,20.318,20.482,19.714]
 ,index: 0
 }
 ]
