bd9c0939ea6aa720c115b9b35b99c2932db39d40
QualityWatch <Feedback@qualitywatch.org.uk>
2016-04-29 11:56:59 +0100

    Update QIAAN140-1.1.js

--- chart/visualisation/QIAAN140-1.1.js
+++ chart/visualisation/QIAAN140-1.1.js
@@ -15,7 +15,7 @@ title: {text:''}
 }
 },
 yAxis: {
-title: {text:'Male age-standardised alcohol-related <br> death rates per 100,000 population <br> in UK', offset: 99}
+title: {text:'Male age-standardised alcohol-related death<br>rates per 100,000 population in UK', offset: 99}
 , max: 50
 , labels: {
 format: '{value:.0f}' 
