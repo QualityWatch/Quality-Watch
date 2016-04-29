f650d16128d78bf8ff31c1c98827fc82e9769c6f
QualityWatch <Feedback@qualitywatch.org.uk>
2016-04-29 13:51:41 +0100

    Update QIAAN139-2.js

--- chart/visualisation/QIAAN139-2.js
+++ chart/visualisation/QIAAN139-2.js
@@ -1,4 +1,5 @@
-/* Created by NCODowd on 27/04/2016 10:33:34 using v2.7 */{
+/* Created by NCODowd on 29/04/2016 13:48:27 using v2.7 */
+{
 title: {text:''},
 credits: {enabled: false},
 exporting: {chartOptions: {
@@ -20,43 +21,43 @@ title: {text:'Proportion of infants breastfed <br> at age 3 months', offset: 87}
 legend: {title: {text: 'Click on series name to show or hide'}, borderWidth: 2},series:[
 {
 name: 'Finland',
-visible: true,type: 'line',
+visible: true,type: 'line', connectNulls : true,
 data: [74,null,null,null,null,76,null,null,null,null,76,null]
 ,index: 1
 },
 {
 name: 'Ireland',
-visible: false,type: 'line',
+visible: false,type: 'line', connectNulls : true,
 data: [null,null,null,null,null,null,null,null,null,21.5,22.7,null]
 ,index: 2
 },
 {
 name: 'Italy',
-visible: false,type: 'line',
-data: [63.5,null,63.5,null,null,59.2,null,null,null,null,null,null]
+visible: false,type: 'line', connectNulls : true,
+data: [63.5,null,63.5,null,null,59.2,null,null,null,null,null]
 ,index: 3
 },
 {
 name: 'Netherlands',
-visible: false,type: 'line',
+visible: false,type: 'line', connectNulls : true,
 data: [46,48,53,52,54,53,48.2,54,54.1,52.2,null,null]
 ,index: 4
 },
 {
 name: 'Spain',
-visible: false,type: 'line',
+visible: false,type: 'line', connectNulls : true,
 data: [null,65.5,null,null,null,null,63.3,null,null,null,null,66.54]
 ,index: 5
 },
 {
 name: 'Sweden',
-visible: false,type: 'line',
+visible: false,type: 'line', connectNulls : true,
 data: [87.7,87.6,87.6,87.2,87,86.1,85.7,84.5,83.7,82.8,81.3,81]
 ,index: 6
 },
 {
 name: 'United Kingdom',
-visible: true,type: 'line',
+visible: true,type: 'line', connectNulls : true,
 data: [28,null,null,null,null,34,null,null,null,null,42,null]
 ,index: 7
 }
