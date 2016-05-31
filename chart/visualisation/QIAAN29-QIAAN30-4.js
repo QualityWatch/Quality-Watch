6fb3fc4297c0913602875e6b32c470c01221e29a
QualityWatch <Feedback@qualitywatch.org.uk>
2016-05-31 09:23:38 +0100

    Changed percentages

--- chart/visualisation/QIAAN29-QIAAN30-4.js
+++ chart/visualisation/QIAAN29-QIAAN30-4.js
@@ -1,42 +1,37 @@
-/* Created by NCODowd on 24/05/2016 16:31:52 using v2.7 */{
+/* Created by NCODowd on 24/05/2016 16:31:52 using v2.7 */
+{
 title: {text:''},
 credits: {enabled: false},
 exporting: {chartOptions: {
-title: {text: 'How has the proportion of carers finding it easy to access information by age changed?'},
+title: {text: 'How has the proportion of carers finding it easy to access information varied by region?'},
 credits: {enabled: true, text: 'Copyright Nuffield Trust & The Health Foundation', href: 'http://www.qualitywatch.org.uk'}
 }},
 xAxis: {
 title: {text:''}
-, categories: ['18-24','25-34','35-44','45-54','55-64','65-74','75-84',85]
+, categories: ['North East','Yorkshire and the Humber','North West','South West','England','South East','East','West Midlands','London','East Midlands']
 , labels: {
  rotation: -90
 , step: 1
 }
 },
 yAxis: {
-title: {text:'Proportion of carers who found it <br> easy to find information and advice', offset: 87}
+title: {text:'Proportion of carers who found it <br> easy to find info and advice', offset: 87}
 , labels: {
 format: '{value:.0f}' 
 ,formatter: function() {return this.value +'%';}}},
 tooltip: {valueSuffix: '%'},
 series:[
 {
-name: '2009-10',
-type: 'column',
-data: [6450,6280,6520,6660,7410,7640,7950,7970]
-,index: 1
-},
-{
 name: '2012-13',
 type: 'column',
-data: [6670,5770,6180,6110,6860,7140,7410,7350]
-,index: 2
+data: [74.8,71.1,69.6,71.2,68.9,67.2,64.2,69.2,64.1,70.8]
+,index: 1
 },
 {
 name: '2014-15',
 type: 'column',
-data: [6130,5980,5760,5860,6370,6890,7030,7130]
-,index: 3
+data: [73.2,68.4,68.2,66.4,65.6,65,63.6,63.4,62.1,61.6]
+,index: 2
 }
 ]
 }
