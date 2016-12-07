fae724307092e2d6b4d9acb9eacabb1751d40719
QualityWatch <Feedback@qualitywatch.org.uk>
2016-12-07 14:18:00 +0000

    Update Nov 16

--- chart/visualisation/QIAAN100-1.js
+++ chart/visualisation/QIAAN100-1.js
@@ -1,4 +1,5 @@
-/* Created by EFisher on 26/10/2015 10:04:00 using v2.7 */{
+/* Created by EFisher on 26/10/2015 10:04:00 using v2.7 */
+{
 title: {text:''},
 credits: {enabled: false},
 exporting: {chartOptions: {
@@ -7,7 +8,7 @@ credits: {enabled: true, text: 'Copyright Nuffield Trust & The Health Foundation
 }},
 xAxis: {
 title: {text:''}
-, categories: ['2007/08','2008/09','2009/10','2010/11','2011/12','2012/13','2013/14','2014/15']
+, categories: ['2007/08','2008/09','2009/10','2010/11','2011/12','2012/13','2013/14','2014/15','2015/16']
 , labels: {
  rotation: -90
 , step: 1
@@ -22,13 +23,13 @@ series:[
 {
 name: 'total - 2 years and over',
 type: 'line',
-data: [55498,36095,25604,21707,18022,14694,13361,14165]
+data: [55498,36095,25604,21707,18022,14694,13362,14192,14139]
 ,index: 1
 },
 {
 name: 'trust apportioned - 2 years and over',
 type: 'line',
-data: [33442,19927,13220,10417,7689,5980,5033,5213]
+data: [33442,19927,13220,10417,7689,5980,5034,5233,5164]
 ,index: 2
 },
 {
