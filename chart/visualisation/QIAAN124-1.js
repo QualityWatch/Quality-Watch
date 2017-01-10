0aef599c449b430567c8ccf26245140b1f0cd79c
QualityWatch <Feedback@qualitywatch.org.uk>
2017-01-10 11:44:13 +0000

    Update QIAAN124-1.js

--- chart/visualisation/QIAAN124-1.js
+++ chart/visualisation/QIAAN124-1.js
@@ -1,6 +1,8 @@
-/* Created by rreed on 05/01/2017 15:02:29 using v2.7 */{
+/* Created by rreed on 05/01/2017 15:02:29 using v2.7 */
+{
 title: {text:''},
 credits: {enabled: false},
+legend: { enabled: false},
 exporting: {chartOptions: {
 title: {text: 'What proportion of adults with learning disabilities live at home?'},
 credits: {enabled: true, text: 'Copyright Nuffield Trust & The Health Foundation', href: 'http://www.qualitywatch.org.uk'}
