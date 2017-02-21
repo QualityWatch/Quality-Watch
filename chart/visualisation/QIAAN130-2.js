5010e7bd53774a07a7b0e44487357dd643f2a8dc
QualityWatch <Feedback@qualitywatch.org.uk>
2017-02-21 12:36:12 +0000

    Update QIAAN130-2.js

--- chart/visualisation/QIAAN130-2.js
+++ chart/visualisation/QIAAN130-2.js
@@ -28,7 +28,7 @@ data: [86.92,74,73.31,73.93,74.22,75.14,76.16,80.9,81.2]
 ,index: 1
 },
 {
-name: 'With hypertension and proteinuria and treated with<br>an angiotensin converting enzyme inhibitor or angiotensin receptor blocker',
+name: 'With hypertension and proteinuria and treated with an angiotensin<br>converting enzyme inhibitor or angiotensin receptor blocker',
 type: 'line',
 data: [null,null,87.3,91.79,90.45,89.51,89.26,91,91.5]
 ,index: 2
