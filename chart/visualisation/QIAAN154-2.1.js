4f5f95f30f3dfae07dca149d0df7226a7cb5e38d
QualityWatch <Feedback@qualitywatch.org.uk>
2017-02-22 10:02:37 +0000

    Update QIAAN154-2.1.js

--- chart/visualisation/QIAAN154-2.1.js
+++ chart/visualisation/QIAAN154-2.1.js
@@ -1,5 +1,5 @@
-/* Created by ianb on 26/06/2015 16:52:26 using v2.6 */{
-title: {text:'Patient-based haemorrhagic stroke, 30-day mortality (in hospital and out of hospital)'},
+/* Created by rreed on 16/02/2017 16:52:26 using v2.6 */{
+title: {text:'Thirty-day mortality after admission to hospital for hemorrhagic stroke based on patient data'},
 credits: {enabled: false},
 exporting: {chartOptions: {
 title: {text: 'How does UK\'s haemorrhagic stroke mortality rate compare internationally over time?'},
@@ -7,7 +7,7 @@ credits: {enabled: true, text: 'Copyright Nuffield Trust & The Health Foundation
 }},
 xAxis: {
 title: {text:''}
-, categories: [2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011]
+, categories: [2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013]
 , labels: {
  rotation: -90
 , step: 1
@@ -22,37 +22,37 @@ legend: {title: {text: 'Click on series name to show or hide'}, borderWidth: 2},
 {
 name: 'Korea',
 visible: true,type: 'line',
-data: [null,null,null,null,22,22.8,22.2,20.3,19.4,18.7,18.7,18.5]
+data: [null,null,null,null,22.0,22.8,22.2,20.8,20.0,19.5,19.4,19.0,18.3,17.4]
 ,index: 1
 },
 {
 name: 'Netherlands',
 visible: false,type: 'line',
-data: [40,40,40.5,37.2,36.4,35.8,34.1,34.1,34.2,32.8,31.7,null]
+data: [40.8,40.8,41.1,38.0,36.9,36.5,33.9,34.1,34.2,32.8,31.8,32.1,null,null]
 ,index: 2
 },
 {
 name: 'New Zealand',
 visible: false,type: 'line',
-data: [35.6,37.8,35.1,33.2,36.8,33.4,36.2,34.8,33.4,32.4,34.1,34.7]
+data: [37.1,38.5,36.4,33.5,36.5,33.8,35.0,33.6,33.4,32.4,33.2,34.2,33.5,32.0]
 ,index: 3
 },
 {
 name: 'Spain',
 visible: false,type: 'line',
-data: [30.3,29.8,30.6,31.4,29.6,30,30,28.9,28.7,28,27.1,26.8]
+data: [30.3,29.8,30.6,31.3,29.6,30.0,30.0,28.9,28.7,28.0,27.1,26.8,26.4,25.9 ]
 ,index: 4
 },
 {
 name: 'Sweden',
 visible: false,type: 'line',
-data: [27.8,26.4,27.7,26,26.3,25.8,25.9,24.4,23,23.3,22.6,24]
+data: [27.7,26.4,27.9,26.0,26.2,25.8,26.0,24.4,23.0,23.4,22.7,24.1,23.3,23.3 ]
 ,index: 5
 },
 {
 name: 'United Kingdom',
 visible: true,type: 'line',
-data: [null,null,null,null,null,null,null,null,35.2,34.8,33.8,33.7]
+data: [null,null,null,null,null,null,null,null,35.2,34.8,33.8,31.0,30.1,28.3]
 ,index: 6
 }
 ]
