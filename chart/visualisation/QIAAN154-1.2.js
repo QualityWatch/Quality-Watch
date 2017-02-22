f1535b82e055caa37cba0dfc64b1748c7fe0dadc
QualityWatch <Feedback@qualitywatch.org.uk>
2017-02-22 09:47:35 +0000

    Update QIAAN154-1.2.js

--- chart/visualisation/QIAAN154-1.2.js
+++ chart/visualisation/QIAAN154-1.2.js
@@ -1,5 +1,4 @@
-/* Created by ianb on 26/06/2015 16:51:27 using v2.6 */{
-title: {text:'Admission-based ischaemic stroke, 30-day in-hospital mortality'},
+/* Created by rreed on 16/02/2017 16:51:58 using v2.6 */{title: {text:'Patient-based ischaemic stroke, 30-day mortality'},
 credits: {enabled: false},
 exporting: {chartOptions: {
 title: {text: 'How does UK\'s ischaemic stroke mortality rate compare internationally over time?'},
@@ -7,101 +6,54 @@ credits: {enabled: true, text: 'Copyright Nuffield Trust & The Health Foundation
 }},
 xAxis: {
 title: {text:''}
-, categories: [2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012]
+, categories: [2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013]
 , labels: {
  rotation: -90
 , step: 1
 }
 },
 yAxis: {
-title: {text:'Age-sex standardised rate per 100 <br> hospital dischages (45+)', offset: 87}
+title: {text:'Age-sex standardised rate per 100 <br> patients (45+)', offset: 87}
 , max: 45
+, min: 0
 , labels: {
 }},
 legend: {title: {text: 'Click on series name to show or hide'}, borderWidth: 2},series:[
 {
-name: 'Australia',
+name: 'Korea',
 visible: true,type: 'line',
-data: [11.9,11.5,11.7,11.4,11.2,11.1,10.8,10.9,11.3,10.5,10.1,10,9.4]
+data: [null,null,null,null,7.7,7.4,7,6.5,6,5.6,5.7,5.6,5.3,4.8]
 ,index: 1
-},
-{
-name: 'Belgium',
-visible: false,type: 'line',
-data: [10.4,10.2,9.9,9.8,9.1,9.3,9,9.1,8.8,9.2,null,null,null]
-,index: 2
-},
-{
-name: 'Canada',
-visible: false,type: 'line',
-data: [null,null,12.9,13.1,12.6,12.1,12.3,12.3,11.5,10.4,10.5,9.7,null]
-,index: 3
-},
-{
-name: 'France',
-visible: false,type: 'line',
-data: [null,null,null,null,null,10.6,null,null,null,8.5,8.5,null,null]
-,index: 4
-},
-{
-name: 'Germany',
-visible: false,type: 'line',
-data: [null,null,null,null,null,8.8,8.1,7.6,7.5,7.8,6.9,6.7,null]
-,index: 5
-},
-{
-name: 'Ireland',
-visible: false,type: 'line',
-data: [14.5,14.3,13.3,12.2,11.1,12.3,11.2,11.9,11.6,10.1,11.1,9.9,null]
-,index: 6
-},
-{
-name: 'Italy',
-visible: false,type: 'line',
-data: [null,8.3,7.9,8.2,7.6,7.6,7.1,6.9,7,6.8,6.5,6.5,null]
-,index: 7
-},
-{
-name: 'Japan',
-visible: false,type: 'line',
-data: [null,null,null,null,null,null,null,null,3.1,null,null,3,null]
-,index: 8
 },
 {
 name: 'Netherlands',
 visible: false,type: 'line',
-data: [16.1,15.6,14.3,12.3,11.6,9.9,9.2,9.3,8.6,8.1,7.5,null,null]
-,index: 9
+data: [17.6,17,16.2,14.2,14.1,12.1,11.5,11.8,11,10.8,10.3,9.6,null,null]
+,index: 2
 },
 {
 name: 'New Zealand',
 visible: false,type: 'line',
-data: [12.4,12.4,11.9,10.7,10.9,10.7,11,10.5,9.5,9.2,8,8.5,null]
-,index: 10
+data: [15.1,14.5,14.5,13.4,13.6,13.9,14,13.3,12.6,12,11.9,13.3,12.1,11]
+,index: 3
 },
 {
 name: 'Spain',
 visible: false,type: 'line',
-data: [12.8,12.3,12.8,12.3,11.7,12.1,11.4,11.4,11,10.6,10.5,10.2,null]
-,index: 11
+data: [13,12.5,13,12.6,11.8,12.3,11.6,11.6,11.2,10.8,10.7,10.4,10.4,9.9]
+,index: 4
 },
 {
 name: 'Sweden',
 visible: false,type: 'line',
-data: [7.9,8,7.9,8,7.6,7.3,7.4,7.3,7.1,6.8,6.6,6.4,null]
-,index: 12
+data: [11.8,11.7,11.4,11.6,11,10.7,10.7,10.6,10.7,10.5,10.2,9.9,9.6,9.6]
+,index: 5
 },
 {
 name: 'United Kingdom',
 visible: true,type: 'line',
-data: [null,null,null,null,null,null,null,null,14.8,12.9,11.7,10.4,null]
-,index: 13
-},
-{
-name: 'United States',
-visible: false,type: 'line',
-data: [5.3,5.3,5.1,4.9,4.9,4.6,4.5,4.2,4.4,null,4.3,null,null]
-,index: 14
+data: [null,null,null,null,null,null,null,null,17,14.7,13.8,11.7,11.6,10.6]
+,index: 6
 }
 ]
 }
