e19799ee9e19d2c71ed4e48a2fa7835825e87b71
QualityWatch <Feedback@qualitywatch.org.uk>
2017-04-20 11:37:07 +0100

    Update QIAAN66-5.2.js

--- chart/visualisation/QIAAN66-5.2.js
+++ chart/visualisation/QIAAN66-5.2.js
@@ -1,4 +1,5 @@
-/* Created by ianb on 24/04/2015 16:51:15 using v2.5 */{
+/* Created by ianb on 24/04/2015 16:51:15 using v2.5 */
+{
 title: {text:''},
 credits: {enabled: false},
 exporting: {chartOptions: {
@@ -7,7 +8,7 @@ credits: {enabled: true, text: 'Copyright Nuffield Trust & The Health Foundation
 }},
 xAxis: {
 title: {text:''}
-, categories: ['2008/09','2009/10','2010/11','2011/12','2012/13','2013/14']
+, categories: ['2008/09','2009/10','2010/11','2011/12','2012/13','2013/14','2014/15','2015/16']
 , labels: {
  rotation: -90
 , step: 1
@@ -25,19 +26,19 @@ series:[
 {
 name: 'Dose 1',
 type: 'column',
-data: [88.1,84.3,88.9,90.6,90.9,91.1]
+data: [88.1,84.3,88.9,90.6,90.9,91.1,89.4,90.2]
 ,index: 1
 },
 {
 name: 'Dose 1&2',
 type: 'column',
-data: [86,82.3,87.5,89.6,89.6,89.8]
+data: [86,82.3,87.5,89.6,89.6,89.8,null,85.1]
 ,index: 2
 },
 {
 name: 'All 3 doses',
 type: 'column',
-data: [80.1,76.4,84.2,86.8,86.1,86.7]
+data: [80.1,76.4,84.2,86.8,86.1,86.7,null,null]
 ,index: 3
 }
 ]
