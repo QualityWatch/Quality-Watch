b198cdc9fa9ae5f8b7ab71c86cb3c102569b6b76
QualityWatch <Feedback@qualitywatch.org.uk>
2017-04-20 13:49:57 +0100

    Update QIAAN80-4.js

--- chart/visualisation/QIAAN80-4.js
+++ chart/visualisation/QIAAN80-4.js
@@ -29,22 +29,10 @@ data: [6.2,5.8,5.8,6.5,6.7,5.6,5.6,5.9,6.0,6.0,6.0,6.5,6.5,6.2,6.1,6.7,6.8,5.9,6
 ,index: 1
 },
 {
-type: 'line',
-marker: {fillColor: 'none', lineColor: null },
-name: 'Incomplete RTT pathways trendline',
-data: [[0, 5.7],[37, 6.7]]
-},
-{
 name: 'Admitted (unadjusted) RTT pathways',
 type: 'line',
 data: [9.7,9.5,9.3,8.5,9.9,10.2,9.2,9.1,9.4,9.5,9.2,9.2,9.8,9.7,9.4,8.8,10.2,10.2,9.3,9.6,10.0,9.8,9.7,9.7,10.5,10.3,10.0,9.3,10.6]
 ,index: 2
-},
-{
-type: 'line',
-marker: {fillColor: 'none', lineColor: null },
-name: 'Admitted (unadjusted) RTT pathways trendline',
-data: [[0, 9],[37, 10]]
 },
 {
 name: 'Non-Admitted RTT pathways',
@@ -52,11 +40,5 @@ type: 'line',
 data: [6.0,5.4,5.4,5.1,6.1,5.2,5.0,5.5,5.5,5.6,5.4,5.6,6.1,5.7,5.7,5.3,6.3,5.4,5.3,5.9,5.8,5.9,5.9,5.9,6.5,6.2,6.0,5.6,6.7]
 ,index: 3
 },
-{
-type: 'line',
-marker: {fillColor: 'none', lineColor: null },
-name: 'Non-Admitted RTT pathways trendline',
-data: [[0, 5.1],[37, 6.1]]
-}
 ]
 }
