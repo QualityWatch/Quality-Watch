20c56ce0e3e08b9bf4b4b8b530bc640df64e502e
QualityWatch <Feedback@qualitywatch.org.uk>
2017-05-24 15:01:07 +0100

    Update QIAAN97-4.js

--- chart/visualisation/QIAAN97-4.js
+++ chart/visualisation/QIAAN97-4.js
@@ -7,12 +7,12 @@ title: {text: 'How has the proportion of staff who feel under pressure to attend
 credits: {enabled: true, text: 'Copyright Nuffield Trust & The Health Foundation', href: 'http://www.qualitywatch.org.uk'}
 }},
 xAxis: {
-categories: [2009,2010,2011,2012,2013,2014,'Question<br>change',2015]
+categories: ['2009','2010','2011','2012','2013','2014','Question<br>change','2015*','2016*']
 , labels: {
 }
 },
 yAxis: {
-title: {text:'Un-weighted percentage of staff <br> feeling pressure in last 3 months <br> to attend work when feeling unwell', offset: 99}
+title: {text:'Un-weighted percentage of staff <br> feeling pressure in last 3 months <br> to attend work when feeling unwell<br>*= weighted score', offset: 99}
 , labels: {
 format: '{value:.1f}' 
 ,formatter: function() {return this.value +'%';}}},
@@ -21,7 +21,7 @@ series:[
 {
   showInLegend: false,
 type: 'column',
-data: [21,22,23,27,25,23,null,56.5]
+data: [21,22,23,27,25,23,null,59,56]
 ,
 }
 ]
