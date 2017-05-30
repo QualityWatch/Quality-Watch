f5138d1363a39a9e9522f5c9ffa80a52ad5fcb38
QualityWatch <Feedback@qualitywatch.org.uk>
2017-05-30 12:37:45 +0100

    Update QIAAN155-1.2.js

--- chart/visualisation/QIAAN155-1.2.js
+++ chart/visualisation/QIAAN155-1.2.js
@@ -1,4 +1,5 @@
-/* Created by EFisher on 31/03/2016 14:51:56 using v2.7 */{
+/* Created by RREED on 30/05/2017 14:51:56 using v2.7 */
+{
 title: {text:''},
 credits: {enabled: false},
 exporting: {chartOptions: {
@@ -7,12 +8,12 @@ credits: {enabled: true, text: 'Copyright Nuffield Trust & The Health Foundation
 }},
 xAxis: {
 title: {text:''}
-, categories: [""]
+, categories: ["2015","2016"]
 , labels: {
 }
 },
 yAxis: {
-title: {text:'Staff satisfaction with the quality <br> of work and care they are able to <br> deliver ', offset: 99}
+title: {text:'Staff satisfaction with the quality <br> of work and care they are able to <br> deliver, weighted scores ', offset: 99}
 , max: 5
 , labels: {
 format: '{value:.0f}' 
@@ -21,31 +22,31 @@ series:[
 {
 name: 'England',
 type: 'column',
-data: [3.89]
+data: [3.30,3.32]
 ,index: 1
 },
 {
 name: 'Acute',
 type: 'column',
-data: [3.95]
+data: [3.30,3.32]
 ,index: 2
 },
 {
 name: 'Ambulance',
 type: 'column',
-data: [3.79]
+data: [3.06,3.10]
 ,index: 3
 },
 {
 name: 'Mental Health & Learning Disability',
 type: 'column',
-data: [3.84]
+data: [3.31,3.35]
 ,index: 4
 },
 {
 name: 'Community',
 type: 'column',
-data: [3.85]
+data: [3.27,3.29]
 ,index: 5
 }
 ]
