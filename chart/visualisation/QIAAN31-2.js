9f572d52ab29c9295adfd0300934e66681035aae
QualityWatch <Feedback@qualitywatch.org.uk>
2017-11-30 09:45:37 +0000

    Update QIAAN31-2.js

--- chart/visualisation/QIAAN31-2.js
+++ chart/visualisation/QIAAN31-2.js
@@ -18,14 +18,14 @@ title: {text:''}
 yAxis: [
     {
     
-title: {text:'Total number of maternities or number of women who had an <br> antenatal assessment by 12 weeks <br> and 6 days of pregnancy (thousands)', offset: 70},
+title: {text:'Total number of maternities or number of women <br> who had an antenatal assessment by 12 weeks <br> and 6 days of pregnancy (thousands)', offset: 70},
 max: 200,
 min: 80,
 tickInterval: 20,
 labels: {}
 },
     {
-title: {text:'Proportion of maternities which had an antenatal assessment <br> by 12 weeks and 6 days of pregnancy', offset: 70},
+title: {text:'Proportion of maternities which had an antenatal <br> assessment by 12 weeks and 6 days of pregnancy', offset: 70},
 max: 110,
 min: 80,
 tickInterval: 5,
