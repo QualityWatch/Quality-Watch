fb3df50150a481910f8b80d37c372a642eef9f97
QualityWatch <Feedback@qualitywatch.org.uk>
2018-08-16 14:39:39 +0100

    Add files via upload

--- chart/visualisation/QIAAN103-1.js
+++ chart/visualisation/QIAAN103-1.js
@@ -1,6 +1,4 @@
-/* Created by JMorris on 09/05/2018 11:23:46 using v2.7 */{
-/* Created by JMorris on 27/11/2017 using v2.7 */
-    title: {
+/* Created by JMorris on 15/08/2018 11:49:02 using v2.7 */{        title: {
             text: ''
         },
         credits: {
@@ -9,7 +7,7 @@
         exporting: {
             chartOptions: {
                 title: {
-                    text: 'Trolley waits: how long to people wait to be admitted onto a hospital ward?'
+                    text: 'How has performance against the four-hour A&E target changed over time?'
                 },
                 credits: {
                     enabled: true,
@@ -22,52 +20,93 @@
             title: {
                 text: ''
             },
-            categories: ['Aug-10', 'Sep-10', 'Oct-10', 'Nov-10', 'Dec-10', 'Jan-11', 'Feb-11', 'Mar-11', 'Apr-11', 'May-11', 'Jun-11', 'Jul-11', 'Aug-11', 'Sep-11', 'Oct-11', 'Nov-11', 'Dec-11', 'Jan-12', 'Feb-12', 'Mar-12', 'Apr-12', 'May-12', 'Jun-12', 'Jul-12', 'Aug-12', 'Sep-12', 'Oct-12', 'Nov-12', 'Dec-12', 'Jan-13', 'Feb-13', 'Mar-13', 'Apr-13', 'May-13', 'Jun-13', 'Jul-13', 'Aug-13', 'Sep-13', 'Oct-13', 'Nov-13', 'Dec-13', 'Jan-14', 'Feb-14', 'Mar-14', 'Apr-14', 'May-14', 'Jun-14', 'Jul-14', 'Aug-14', 'Sep-14', 'Oct-14', 'Nov-14', 'Dec-14', 'Jan-15', 'Feb-15', 'Mar-15', 'Apr-15', 'May-15', 'Jun-15', 'Jul-15', 'Aug-15', 'Sep-15', 'Oct-15', 'Nov-15', 'Dec-15', 'Jan-16', 'Feb-16', 'Mar-16', 'Apr-16', 'May-16', 'Jun-16', 'Jul-16', 'Aug-16', 'Sep-16', 'Oct-16', 'Nov-16', 'Dec-16', 'Jan-17','Feb-17', 'Mar-17', 'Apr-17', 'May-17', 'Jun-17', 'Jul-17', 'Aug-17', 'Sep-17', 'Oct-17', 'Nov-17', 'Dec-17', 'Jan-18', 'Feb-18', 'Mar-18'],
+            categories: ['2008/09 Q1','2008/09 Q2','2008/09 Q3','2008/09 Q4','2009/10 Q1','2009/10 Q2','2009/10 Q3','2009/10 Q4','2010/11 Q1','2010/11 Q2','2010/11 Q3','2010/11 Q4','2011/12 Q1','2011/12 Q2','2011/12 Q3','2011/12 Q4','2012/13 Q1','2012/13 Q2','2012/13 Q3','2012/13 Q4','2013/14 Q1','2013/14 Q2','2013/14 Q3','2013/14 Q4','2014/15 Q1','2014/15 Q2','2014/15 Q3','2014/15 Q4','2015/16 Q1','2015/16 Q2','2015/16 Q3','2015/16 Q4','2016/17 Q1','2016/17 Q2','2016/17 Q3','2016/17 Q4','2017/18 Q1','2017/18 Q2','2017/18 Q3','2017/18 Q4','2018/19 Q1'],
             labels: {
-                rotation: -90,
-                step: 4
-            },
-            tickInterval: 2
+                rotation: -45,
+                step: 2
+            }
         },
         yAxis: [{
             title: {
-                text: 'Percentage of admitted patients waiting >4 hours <br> from decision to admit to admission',
-                offset: 60
+                text: 'Number of attendances (millions)'
             },
-            max: 20,
-  min: 0,          labels: {
-                format: '{value:.1f}',
-                formatter: function () {
-                    return this.value + '%';
-                }
+           min: 0,
+            max: 10,
+          tickInterval: 2,
+            labels: {
+                format: '{value:.0f}'
             }
         }, {
             title: {
-                text: 'Number of admitted patients waiting <br> >12 hours from decision to admit to admission',
-                offset: 60
-            },
+                text: 'Percentage of patients spending <br> <4 hours in A&E',
+  offset: 55,             },
             opposite: true,
-            max: 1200,
-  min: 0,          labels: {
-                format: '{value:.1f}',
+            min: 50,
+            max: 100,
+            tickInterval: 10,
+            labels: {
+                format: '{value:.0f}',
                 formatter: function () {
-                    return this.value;
-                }
+                    return this.value + '%';
+                     }
             }
-        }
-
-        ],
-        
+        }],
+        plotOptions: {    
+    column: {           
+    stacking: 'normal',
+       groupPadding: 0.5,   // Exactly overlap
+        pointWidth: 10                                     
+    }
+},
         series: [{
-            tooltip: {
-            valueSuffix: '%'
-        },name: 'Percentage waiting >4 hours',
-            type: 'line',
-            yAxis: 0,
-            data: [0.87,1.39,1.59,1.67,3.05,2.98,1.96,1.93,1.80,1.71,1.71,1.33,1.22,1.82,2.09,2.10,2.48,3.24,3.73,2.24,2.39,1.96,1.97,1.63,1.68,2.14,2.48,2.66,3.66,4.51,4.69,5.38,5.47,2.21,1.98,2.10,2.45,3.13,3.08,2.70,3.27,4.15,4.57,3.23,3.53,3.82,3.27,3.59,3.56,4.40,5.31,5.53,9.49,9.69,7.80,6.48,6.22,5.02,4.16,3.65,4.40,5.58,6.58,7.19,6.75,10.64,10.82,10.86,8.39,7.75,7.35, 7.64, 7.28, 7.50, 9.90,10.83,12.40,16.16, 12.17,8.43,7.20,7.97,7.02,7.47,7.65,8.66,8.86,9.42,13.28,15.40,14.41,14.45] 
-        }, {
-            name: 'Number of extreme waits',
+            name: 'A&E attendances (type 2 & 3)',
             type: 'column',
+            data:
+[1.57,1.57,1.51,1.51,1.75,1.76,1.70,1.69,1.92,1.87,1.82,1.83,1.92,1.88,1.82,1.86,1.96,1.91,1.81,1.81,1.93,1.94,1.82,1.88,2.03,1.93,1.91,1.90,2.01,1.97,1.93,2.05,2.05,2.08,2.00,1.97,2.11,2.06,2.13,2.16,2.31], color: '#3FCFD5'
+        }, {
+            name: 'A&E attendances (type 1)',
+            type: 'column',
+            data:[3.42,3.36,3.35,3.30,3.55,3.42,3.42,3.22,3.60,3.50,3.43,3.40,3.59,3.47,3.43,3.53,3.62,3.58,3.55,3.50,3.62,3.59,3.48,3.53,3.77,3.67,3.67,3.48,3.70,3.71,3.73,3.82,3.85,3.89,3.85,3.67,3.90,3.87,3.90,3.71,3.92], color: '#E59AAA'
+        }, {
+         tooltip: {
+            valueSuffix: '%'},
+            name: 'Percentage in 4 hours or less (type 1)',
+            type: 'line',
+                marker:{enabled:false},
             yAxis: 1,
-            data: [1, 0, 0, 2, 15, 17, 3, 1, 6, 3, 10, 6, 1, 9, 42, 28, 4, 8, 6, 1, 0, 0, 2, 1, 2, 2, 0, 1, 14, 50, 17, 81, 43, 0, 7, 3, 2, 9, 21, 7, 21, 42, 77, 10, 23, 10, 8, 5, 8, 28, 55, 35, 186, 650, 184, 54, 58, 46, 25, 22, 28, 23, 58, 29, 29, 158, 188, 349, 196, 107, 84, 113, 57, 86, 252, 457, 553, 989, 338, 270, 142, 114, 55, 74, 50, 77, 54, 107, 500, 1043, 371, 853]
-        }] }
+          
+            data: [97.6, 98.1, 96.4, 96.8, 98.0, 98.1, 96.8, 96.9, 97.7, 97.0, 94.7, 94.8, 95.6, 95.9, 94.5, 93.7, 94.9, 95.4, 93.5, 91.1, 93.4, 94.4, 93.5, 92.7, 92.6, 92.5, 88.9, 87.5, 91.1, 91.4, 87.4, 81.8, 85.4, 85.9, 81.9, 81.4, 85.5, 85.2, 81.7, 76.8, 84.4], color: '#E10E49'
+        }, {
+         tooltip: {
+            valueSuffix: '%'},
+            name: 'Percentage in 4 hours or less (type 2 & 3)',
+            type: 'line',
+                marker:{enabled:false},
+            yAxis: 1,
+            data: [99.9, 99.9, 99.9, 99.9, 99.8, 99.9, 99.9, 99.9, 99.9, 99.9, 99.8, 99.8, 99.8, 99.9, 99.8, 99.8, 99.8, 99.9, 99.8, 99.8, 99.8, 99.8, 99.8, 99.8, 99.7, 99.7, 99.6, 99.7, 99.6, 99.6, 99.5, 99.2, 99.4, 99.5, 99.4, 99.3, 99.2, 99.4, 99.2, 99.0, 99.2], color: '#009AA6'
+        }, {
+         tooltip: {
+            valueSuffix: '%'},
+            name: 'Old target',
+            type: 'line',
+                            marker: {
+                    enabled: false
+                },
+            yAxis: 1,
+               dashStyle:'Dash',
+            marker:{enabled:false},
+            data: [98, 98, 98, 98, 98, 98, 98, 98, 98, 98, 98, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,null, null, null, null, null,null,null,null, null], color: '#6C6F70'
+        }, {
+         tooltip: {
+            valueSuffix: '%'},
+            name: 'New target',
+            type: 'line',
+                            marker: {
+                    enabled: false
+                },
+            yAxis: 1,
+               dashStyle:'Dash',
+            marker:{enabled:false},
+            data: [null, null, null, null, null, null, null, null, null, null, null, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95, 95,95, 95, 95, 95,95,95,95], color: '#4F0B7B'
+        }]
+    }
